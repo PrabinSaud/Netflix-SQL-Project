d75413d35d99ed7e7f3f55cf6d3334208d65098a
prabin Saud <prabinsaud2002@gmail.com>
2025-12-11 22:00:02 +0530

    analysis cast from netflixdata

---
+++ Query/Director and Cast.sql
@@ -0,0 +1,12 @@
+/* -------------------------------------------------------------
+   MODULE 6: DIRECTOR & CAST ANALYSIS
+   -------------------------------------------------------------
+   GOALS:
+   - Most frequent directors
+   - Most frequent actors
+   - Count titles per director
+   - Actor co-appearance patterns (optional)
+   - Director-country breakdown
+
+   Write all director & cast queries below.
+*/
